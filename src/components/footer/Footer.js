fe454195331f3cd530d0b9066aa92a974f3b3e89
Tural <tural735m@gmail.com>
2022-02-07 18:07:25 +0400

    finish website

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './footer.scss';
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+import { Link } from 'react-router-dom';
+const Footer = () => {
+  return <div className='footer' style={{backgroundImage: `url(${bg})`}}>
+    <div className="footer__content container">
+      <div className="footer__content__logo">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <Link to="/">TMovies</Link>
+        </div>
+      </div>
+      <div className="footer__content__menus">
+        <div className="footer__content__menu">
+          <Link to="/">Home</Link>
+          <Link to="/">Contact Us</Link>
+          <Link to="/">Term of Service</Link>
+          <Link to="/">About Us</Link>
+        </div>
+        <div className="footer__content__menu">
+          <Link to="/">Live</Link>
+          <Link to="/">FAQ</Link>
+          <Link to="/">Premium</Link>
+          <Link to="/">Pravicy Policy</Link>
+        </div>
+        <div className="footer__content__menu">
+          <Link to="/">You must watch</Link>
+          <Link to="/">Recent release</Link>
+          <Link to="/">Top IMDB</Link>
+        </div>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Footer;
---
+++ src/config/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route,Switch } from 'react-router-dom';
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/detail/Detail';
+import Home from '../pages/Home';
+
+const Routes = () => {
+  return <Switch>
+    <Route path="/:category/search/:keyword" component={Catalog}/>
+    <Route path="/:category/:id" component={Detail}/>
+    <Route path="/:category" component={Catalog}/>
+    <Route path="/" exact component={Home}/>
+  </Switch>
+};
+
+export default Routes;
