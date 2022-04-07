096eb10dcae9df7c0c011025fc07e2f496006900
Tural <tural735m@gmail.com>
2022-04-07 18:04:05 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Routes from './config/Routes';
 
+
 function App() {
   return (
       <BrowserRouter>
--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig={
-  baseUrl:"https://api.themoviedb.org/3/",
-  apiKey: "1cc28d7cb8202fa7566afa90c4a8b9f4",
+  baseUrl:`${ process.env.REACT_APP_BASE_URL }`,
+  apiKey: `${ process.env.REACT_APP_API_KEY }`,
   originalImage:(imgPath)=>`https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image:(imgPath)=>`https://image.tmdb.org/t/p/w500/${imgPath}`,
 
